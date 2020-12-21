51d2c85bf553400c02bf04868234b094b553f47c
JasonLi2 <thatjasonli@gmail.com>
2020-12-21 15:25:09 -0700

    Added html and js file
    Able to increase P1 and P2 scores as well as reset

---
+++ app.js
@@ -0,0 +1,52 @@
+const buttonP1  = document.querySelector("#buttonP1");
+const buttonP2  = document.querySelector("#buttonP2");
+const trackerP1 = document.querySelector("#trackerP1");
+const trackerP2 = document.querySelector("#trackerP2");
+const resetButton = document.querySelector("#resetButton");
+const maxScoreSelect = document.querySelector("#scoreSelect");
+
+let scoreP1 = 0;
+let scoreP2 = 0;
+let maxScore = 5;
+let gameOver = false;
+
+// Logic for increasing p1's score
+buttonP1.addEventListener("click", function ()  {
+    if (!gameOver) {
+        scoreP1 += 1;
+        if(scoreP1 === maxScore) {
+            gameOver = true;
+        }
+        trackerP1.textContent = scoreP1;
+    }
+})
+
+// Logic for increasing p2's score
+buttonP2.addEventListener("click", function ()  {
+    if (!gameOver) {
+        scoreP2 += 1;
+        if(scoreP2 === maxScore) {
+            gameOver = true;
+        }
+        trackerP2.textContent = scoreP2;
+    }
+})
+
+//logic for the reset button
+resetButton.addEventListener("click", reset)
+
+//logic for selecting the score to play to
+maxScoreSelect.addEventListener("change", function () {
+    maxScore = parseInt(this.value);
+    reset();
+})
+
+//reset function that will return everything to starting state
+function reset() {
+    gameOver = false;
+    scoreP1 = 0;
+    scoreP2 = 0;
+
+    trackerP1.textContent = scoreP1;
+    trackerP2.textContent = scoreP2;
+}
